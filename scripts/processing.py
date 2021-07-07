e21b54655cc695d37f94df0951f3dae0e2355eb9
alejandro <aservet1@binghamton.edu>
2021-07-06 20:55:29 -0400

    reformatting

--- scripts/processing.py
+++ scripts/processing.py
@@ -1,21 +1,17 @@
 '''
-define token:
-	lex : whatever it is
-	kind : plainText | replaceText | commentText
-		
-grammar:
-	comments are stored as tokens, but discarded upon processing
-	plain text, interspersed with $${replaceText} markers.
-	replaceText has the name of a JSON field, and optionally a specifier string, delimited by a |, which maps to a function for how to process the JSON field's corresponding value
-
+	define token:
+		lex : whatever it is
+		kind : plainText | replaceText | commentText
+			
+	grammar:
+		comments are stored as tokens, but discarded upon processing
+		plain text, interspersed with $${replaceText} markers.
+		replaceText has the name of a JSON field, and optionally a specifier string, delimited by a |, which maps to a function for how to process the JSON field's corresponding value
 	
-
-
-replaceText: /$${.*}/
-	($ and {} arent special regex characters, in this pseudocode at least. confirm the python regex syntax.)
-
-plainText: /[^$]*/ (anything without a dollar sign, so when you reach a dollar sign, it might not be special...but in that case it'll just get read as another plaintext string, and will get seamlessly joined with the last one during the processing phase)
-
+	replaceText: /$${.*}/
+		($ and {} arent special regex characters, in this pseudocode at least. confirm the python regex syntax.)
+	
+	plainText: /[^$]*/ (anything without a dollar sign, so when you reach a dollar sign, it might not be special...but in that case it'll just get read as another plaintext string, and will get seamlessly joined with the last one during the processing phase)
 '''
 
 ''' {-. --. ---. -----.) pseudocode (.----- .--- .-- .-} '''
@@ -35,7 +31,6 @@ class Token:
 		self.kind = kind
 	def __str__(self):
 		return f'(lex: {self.lex}, kind: {self.kind})'
-
 
 def scan(text):
 	def next_token(text):
@@ -59,25 +54,23 @@ def scan(text):
 			
 	token_stream = []
 	while len(text):
-		token_stream.append(next_token(text))
+		token = next_token(text)
 		text = text[len(token_stream[-1].lex):]
+		token_stream.append(text)
 	return token_stream
-
 
 def getFn(function):
 	def extract_figure_number(text):
 		return ''.join(c for c in text if c.isdigit()) ## TODO make sure this is the accurate way to parse figure number. is everything other than the fignum going to be non-numeric?
-
 	def html_list_to_text_list(text):
 		return 'some BeautifulSoup parsing thing'
-
 	def extract_chapter_number(text): # format: 'Chapter N: XXXX', extracting N
 		return ''.join([ c for c in text.split(':')[0] if c.isdigit() ])
 
-	fns = locals()
+	fns = locals(); del fns['function']
 
 	if not function in fns.keys():
-		print('invalid function specifier!!', function)
+		print('invalid function name!!', function)
 		exit(2)
 
 	return fns[function]
@@ -85,14 +78,14 @@ def getFn(function):
 def process(tok):
 	if tok.kind == 'commentText':
 		return ''
-	if tok.kind == 'plainText':
+	elif tok.kind == 'plainText':
 		return tok.lex
 	elif tok.kind == 'replaceText':
 		lex = tok.lex.replace(OPENER,'').replace(CLOSER,'')
 		if '|' not in lex:
 			try:
 				return JSON_DATA[lex]
-			except KeyError: # make sure this trycatch isnt necessary, its just a stopper for a JSON key not existing right now
+			except KeyError: # TODO make sure this trycatch isnt necessary, its just a stopper for a JSON key not existing right now
 				print(' .))-) key not found in JSON:',lex)
 				return tok.lex
 		else:
@@ -112,5 +105,4 @@ def process_token_stream(token_stream):
 def process_text(text, json_data):
 	global JSON_DATA; JSON_DATA = json_data
 	return process_token_stream(scan(text))
-
 
