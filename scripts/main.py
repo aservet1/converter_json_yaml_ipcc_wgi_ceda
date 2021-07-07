d71bcf237ba662b0abae2c6f5df8d6ad42b52c35
alejandro <aservet1@binghamton.edu>
2021-07-06 21:36:52 -0400

    get rid of my_globals module

--- scripts/main.py
+++ scripts/main.py
@@ -16,7 +16,6 @@ with open(template_file) as fd:
 	template_data = fd.read()
 with open(json_file) as fd:
 	json_data = json.load(fd)[0] # el JSON siempre va a ser una lista con un elemento?
-	my_globals.initialize_jsondata(json_data) # TODO honestly this might be a janky way to handle it, you could also just pass the JSON dict in to the 'overall process text' function you end up coming up with
 
 processed = process_text(template_data, json_data)
 
