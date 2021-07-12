2b91c3c7be99ed79cffc7e6588ad6fe8b2334772
alejandro <aservet1@binghamton.edu>
2021-07-12 15:29:45 -0400

    misc housekeeping

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,3 +1,4 @@
+
 def extract_figure_number(text):
 	return ''.join(c for c in text if c.isdigit()) ## TODO make sure this is the accurate way to parse figure number. is everything other than the fignum going to be non-numeric?
 
@@ -8,10 +9,11 @@ def extract_chapter_number(text): # format: 'Chapter N: XXXX', extracting N
 	return ''.join([ c for c in text.split(':')[0] if c.isdigit() ])
 
 def BLANK(text):
-	return "1234567890"
+	return "___blankkk___"
 
 def extract_section_number(text):
 	return "TO-IMPLEMENT: extract_section_number"
 
 def get_author_names(text):
 	return "[ {\"TO-IMPLEMENT\": \"get_author_names\"} ]"
+
