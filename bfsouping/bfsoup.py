297268d63a99a0f51523f91dfb8b597e1bd82efb
alejandro <aservet1@binghamton.edu>
2021-07-22 21:07:01 -0400

    working with bfsoup

---
+++ bfsouping/bfsoup.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+
+string =
+
+print(BeautifulSoup(string, features="lxml"))
---
+++ bfsouping/html_to_txt.py
@@ -0,0 +1,15 @@
+from sys import argv
+from bs4 import BeautifulSoup
+
+try:
+	src = argv[1]
+	dest = argv[2]
+except IndexError:
+	print("usage:",argv[0],"src-html dest-txt")
+
+with open(src) as fd:
+	text = BeautifulSoup(fd.read()).get_text()
+with open(dest,'w') as fd:
+	fd.write(text)
+
+print('done')
